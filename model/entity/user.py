c5c68dc0d59bf0e7577c48f6030221e520e3e9e4
NADMI <rnardestani@gmail.com>
2025-07-03 19:53:07 +0330

    UPDATE

--- model/entity/user.py
+++ model/entity/user.py
@@ -1,17 +1,12 @@
 from model.tools.validator import *
 
 
-class Ticket:
-    def __init__(self, code, name, family, origin, destination, start_date_time, end_date_time, ticket_type, price):
+class User:
+    def __init__(self, code, name, family, username,password,):
         self.code = code
         self.name = name
         self.family = family
-        self.origin = origin
-        self.destination = destination
-        self.start_date_time = start_date_time
-        self.end_date_time = end_date_time
-        self.ticket_type = ticket_type
-        self.price = price
+
 
     def __repr__(self):
         return f"{self.__dict__}"
