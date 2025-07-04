06385e7eca47ad3e4d62c4951b35768a876e01d6
rnard <rnardestani@gmail.com>
2025-07-04 20:10:46 +0330

    UPDATE

--- model/entity/user.py
+++ model/entity/user.py
@@ -1,4 +1,6 @@
-from model.tools.validator import *
+from model.validator import *
+
+from model.validator import code_validator, name_validator, family_validator
 
 
 class User:
@@ -25,5 +27,56 @@ class User:
 
     @code.setter
     def code(self, value):
-        code_Validator(code)
-        self._code = code
+        code_validator(value)
+        self._code = value
+
+    @property
+    def name(self):
+        return  self._name
+
+    @name.setter
+    def name(self, value):
+        name_validator(value)
+        self._name = value
+
+    @property
+    def family(self):
+        return  self._family
+
+    @family.setter
+    def family(self, value):
+        family_validator(value)
+        self._family = value
+
+    @property
+    def username(self):
+        return  self._username
+
+    @username.setter
+    def username(self, value):
+        username_validator(value)
+        self._username = value
+
+    @property
+    def password(self):
+        return  self._password
+
+    @password.setter
+    def password(self, value):
+        password_validator(value)
+        self._password = value
+
+    @property
+    def role(self):
+        return  self._role
+
+    @role.setter
+    def role(self, value):
+        role_validator(value)
+        self._role = value
+
+
+
+
+
+
