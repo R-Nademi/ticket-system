84b5d7820ee146654f72c06312dfcf3ef4bdeab7
rnard <rnardestani@gmail.com>
2025-07-09 17:10:00 +0330

    UPDATE

--- test/ticket_test.py
+++ test/ticket_test.py
@@ -3,7 +3,8 @@ from model.entity.ticket import Ticket
 
 ticket = Ticket(1, "Ali", "AliPour",
                    "1360", "Mashhad",
-           "Tehran", "10.30", "12.00","Airline","30,$")
+           "Tehran", "10.30", "12.00",
+                "Airline","A21","30$")
 
 
 print(ticket)
